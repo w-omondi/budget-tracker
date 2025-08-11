007ffdbc58433f7c43a102a45ec3aea4457520ad
w-omondi <washingndi@gmail.com>
2025-08-11 11:47:34 +0300

    structure initial commit

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main;
+
+func main(){
+	// This is the main function of the Go program.
+	// You can add your code here to execute when the program runs.
+	println("Hello, World!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/w-omondi/budget-tracker.git
+
+go 1.24.5
